f012421ebaf8d7c6ee252eaf5c71a0c84c0799cc
Asuka124 <hz222bp@student.lnu.se>
2020-09-25 16:00:46 +0200

    1st

---
+++ wordgame_1.py
@@ -0,0 +1,9 @@
+print('---------Python Day1----------')
+temp = input("Rewards if you input 42:")
+guess = int(temp)
+if guess == 42:
+    print("Gratis")
+    print("Here is the rewards: ❤")
+else:
+    print("I told you")
+print("you are free to go")
