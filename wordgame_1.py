55f7f411230373736f700c3350454af005e743f2
Asuka124 <hz222bp@student.lnu.se>
2020-09-27 18:09:01 +0200

    random while

--- wordgame_1.py
+++ wordgame_1.py
@@ -1,9 +1,21 @@
-print('---------Python Day1----------')
-temp = input("Rewards if you input 42:")
-guess = int(temp)
-if guess == 42:
-    print("Gratis")
-    print("Here is the rewards: ❤")
-else:
-    print("I told you")
-print("you are free to go")
+""" ---------Python Day2---------- """
+import random
+
+counts = 3
+answer = random.randint(0, 42)
+while (counts > 0):
+    temp = input("Rewards if you know the random number:")
+    guess = int(temp)
+    if guess == answer:
+        print("Correct")
+        print("Here is the rewards: ❤")
+        break
+    else:   
+        print("I told you")
+        if guess < answer:
+            print("bigger")
+        else:
+            print("smaller")
+        counts =counts - 1
+print("you are free to go. The answer is " + str(answer))
+ 
